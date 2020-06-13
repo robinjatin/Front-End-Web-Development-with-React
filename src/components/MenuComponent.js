dfde5c0e352a74a53a60ddffadca8a26907c4cd0
robinjatin <jatinvirsingh4925@gmail.com>
2020-06-13 23:06:38 +0530

    eighth commit

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {Card,CargImg, CardImgOverlay,CardText,CardBody,CardTitle, CardImg} from 'reactstrap';
-
+import {Card,CargImg, CardImgOverlay,CardText,CardBody,CardTitle, CardImg, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 
     //Definition of constructor of this component 
     
     //Change the state of the componente like this:
-    function RenderMenuItem({dish, onClick}){
-        return(
-            <Card onClick={()=>onClick(dish.id)}>
-            <CardImg width="100%" object src={dish.image} alt = {dish.name}/>
-            <CardImgOverlay body className="ml-5">
-                <CardTitle >
-                    {dish.name}
-                </CardTitle>
-            </CardImgOverlay>
-        </Card>
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
+            </Card>
         );
     }
 
@@ -23,7 +23,7 @@ import {Card,CargImg, CardImgOverlay,CardText,CardBody,CardTitle, CardImg} from 
             const menu = props.dishes.map((dish)=>{
                 return (
                     <div key={dish.id} className="col-12 col-md-5 m-1">
-                       <RenderMenuItem dish={dish} onClick={props.onClick} />
+                       <RenderMenuItem dish={dish}/>
                     </div>
                 );
             });
@@ -31,9 +31,17 @@ import {Card,CargImg, CardImgOverlay,CardText,CardBody,CardTitle, CardImg} from 
             return (
                 <div className="container">
                     <div className="row">
-                        {menu}
+                        <Breadcrumb>
+                        <BreadcrumbItem><Link to='home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>Menu</h3>
+                            <hr />
+                        </div>
                     </div>
                     <div className="row">
+                        {menu}
                     </div>
                 </div>
             );
